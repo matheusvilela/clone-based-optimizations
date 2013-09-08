57013d83232e51dd0a4438024cca1b9697deef7b
matheusvilela@gmail.com <matheusvilela@gmail.com@739d9dac-3fae-b4a2-00e6-ba043a5556d3>
2013-09-08 15:03:02 +0000

    Using PADriver on dead store elimination opt.

--- trunk/remove-dead-stores/DeadStoreElimination.h
+++ trunk/remove-dead-stores/DeadStoreElimination.h
@@ -13,52 +13,59 @@
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Analysis/AliasSetTracker.h"
-#include "llvm/Analysis/AliasAnalysis.h"
-#include "llvm/Analysis/MemoryBuiltins.h"
+#include "llvm/Transforms/Utils/Cloning.h"
 #include "PADriver.h"
 
 namespace llvm {
-  STATISTIC(RemovedStores, "Number of removed stores.");
+  STATISTIC(RemovedStores,   "Number of removed stores.");
+  STATISTIC(FunctionsCount,  "Total number of functions.");
+  STATISTIC(FunctionsCloned, "Number of functions cloned.");
+  STATISTIC(ClonesCount,     "Number of functions that are clones.");
+  STATISTIC(CallsCount,      "Total number of calls.");
+  STATISTIC(PromissorCalls,  "Number of promissor calls.");
+  STATISTIC(CallsReplaced,   "Number of calls replaced.");
+ 
   class DeadStoreEliminationPass : public ModulePass {
 
     // Successors and Predecessor basic blocks on the CFG
     std::map<BasicBlock*, std::vector<BasicBlock*> > successors;
     std::map<BasicBlock*, std::vector<BasicBlock*> > predecessors;
 
-    //TODO:
-    // Functions that store on params and can potentially be optimized
-    // std::map<Function*, std::set<Value*> > functionsThatStoreOnParams;
-    // * Run intraprocedural dead store elimination. While doing this:
-    //   - Get function that store on args and these args
-    //   - If a store on an arg cannot be removed by rule 3,
-    //     remove it from the args that are stored
-    // * Take the possible stores out of the call.
-    // * Run intraprocedural dead store elimination
-    //   + If an artificial store can be removed, then:
-    //     - Clone the function, removing the store
-    //     - Change the call
+    // Functions that store on arguments
+    std::map<Function*, std::set<Value*> > fnThatStoreOnArgs;
 
-    //PADriver* PAD;
-    AliasAnalysis *AA;
+    // Positions that global vars points to
+    std::set<int> globalPositions;
+
+    std::map< Instruction*, std::set<Value*> > deadArguments;
+    std::map<Function*, std::vector<Instruction*> > fn2Clone;
+
+    PADriver* PAD;
     Function *currentFn;
 
    public:
     static char ID;
 
     // IN and OUT sets for the Dead Store Analysis
-    std::map<const Instruction*, AliasSetTracker* > inValues;
-    std::map<const Instruction*, AliasSetTracker* > outValues;
+    std::map<const Instruction*, std::set<int> > inValues;
+    std::map<const Instruction*, std::set<int> > outValues;
 
     DeadStoreEliminationPass();
 
+    std::set<int> getRecursivePositions(int id);
+    void getGlobalVarsInfo(Module &M);
+    bool canBeRemoved(Value* ptr, Instruction* inst, Function &F, bool verifyArgs);
+    bool cloneFunctions();
+    Function* cloneFunctionWithoutDeadStore(Function *Fn, Instruction* caller, std::string suffix);
+    void replaceCallingInst(Instruction* caller, Function* fn);
+    void getFnThatStoreOnArgs(Module &M);
     virtual void getAnalysisUsage(AnalysisUsage &AU) const;
     bool runOnModule(Module &M);
-    bool runOnFunction(Function &F);
-    bool removeDeadStores(BasicBlock &BB, Function &F);
+    void runDeadStoreAnalysis(Function &F);
+    bool removeDeadStores(Function &F);
     bool analyzeBasicBlock(BasicBlock &BB);
     void print(raw_ostream &O, const Module *M) const;
     void printAnalysis(raw_ostream &O) const;
-    void printSet(raw_ostream &O, AliasSetTracker &myset) const;
+    void printSet(raw_ostream &O, const std::set<int> &myset) const;
   };
 }
